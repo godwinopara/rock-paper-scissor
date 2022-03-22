60fd49e47b6fd66ea62beb7ce8ad18c75646e18a
godwin opara <godwinopara62@yahoo.com>
2022-03-22 13:29:46 +0100

    added a function displayResultInDom that updates the score to the dom

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const gameResult = document.getElementById("game-result");
+let score = document.querySelector(".score");
+const restart = document.querySelector("input");
+let player = 0;
+let computer = 0;
+let result = "";
+let gameplayed = 0;
 
 // This function return a random choice between ROCK, PAPER OR SCISSORS for the computer
 function computerPlay() {
@@ -14,7 +20,6 @@ function decideWinner(playerChoice, computerChoice) {
   const scissors = "Scissors Cuts Paper";
 
   let winner = "";
-  let result = "";
 
   if (playerChoice === "rock") {
     if (computerChoice === "scissors") {
@@ -51,18 +56,93 @@ function decideWinner(playerChoice, computerChoice) {
     }
   }
 
-  // update the dom with the winner of the game
-  gameResult.textContent = result;
+  // increment the game played for every single round
+  gameplayed++;
+
+  if (gameplayed === 5) {
+    gameOver();
+    winnerAfter5rounds(player, computer);
+  }
+
+  // update the dom with the result of the game
+  displayResultInDom(winner, result);
+
   return [winner, result];
+}
+
+function winnerAfter5rounds(player, computer) {
+  if (player > computer) {
+    result = "Congratulation You Won This Round";
+  } else if (player < computer) {
+    result = "Sorry You Lost This Round To Computer";
+  } else {
+    result = "It's a draw";
+  }
+}
+
+function displayResultInDom(winner, result) {
+  // display winning message on dom
+  gameResult.textContent = result;
+
+  // updates the score on the dom
+  switch (winner) {
+    case "player":
+      player += 1;
+      score.textContent = `Score: Player:${player} Computer:${computer}`;
+      break;
+    case "computer":
+      computer += 1;
+      score.textContent = `Score: Player:${player} Computer:${computer}`;
+      break;
+  }
+}
+
+function gameOver() {
+  let gameChoiceBtns = document.querySelectorAll("button");
+
+  // disable the button when the game is over
+  gameChoiceBtns.forEach((btn) => {
+    btn.disabled = true;
+  });
+
+  // display the restart button
+  restart.style.display = "block";
+
+  // An eventlistener that restart the game when the restart button is clicked
+  restart.addEventListener("click", () => {
+    restartGame();
+  });
+}
+
+function restartGame() {
+  //hide the restart button
+  restart.style.display = "none";
+
+  player = 0;
+  computer = 0;
+  gameplayed = 0;
+  result = "";
+  gameResult.textContent = result;
+
+  // update the score back to player: 0, computer 0
+  score.textContent = `Score: Player:${player} Computer:${computer}`;
+
+  // Enable the button to be click able again
+  let gameChoiceBtns = document.querySelectorAll("button");
+  gameChoiceBtns.forEach((btn) => {
+    btn.disabled = false;
+  });
 }
 
 function game() {
   let gameChoiceBtns = document.querySelectorAll("button");
+
   gameChoiceBtns.forEach((gameChoiceBtn) => {
     gameChoiceBtn.addEventListener("click", () => {
-      console.log(decideWinner(gameChoiceBtn.textContent.toLowerCase(), computerPlay()));
+      decideWinner(gameChoiceBtn.textContent.toLowerCase(), computerPlay());
     });
   });
 }
 
+// run the game
 game();
